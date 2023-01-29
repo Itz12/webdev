41b01e1f0f6aa14aba1d0cdbed72cf15f88aa64c
itz12 <ifeanyitrust8@gmail.com>
2023-01-29 17:05:31 +0100

    qne

---
+++ index.js
@@ -0,0 +1,72 @@
+/*let xp = 0;
+
+let health = 100;
+
+let gold = 50;
+
+let currentWepon = 0;
+
+let fighting;
+let monterHealth;
+let inventory = ["stick"];
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const Text = document.querySelector("#Text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldTest = document.querySelector("#goldText");
+const monsterstats = document.querySelector("#monsterstats");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const locations =[
+    {
+        name:"town square",
+        "button text":[]
+    }
+]
+
+// initialize button
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location){
+
+}
+
+function goStore() {
+  button1.innerText="buy 10 health(10 gold)";
+  button2.innerText="buy weapon(30 gold)";
+  button3.innerText="Go to town square";
+  button1.innerText=buyHealth;
+  button2.innerText=buyWeapon;
+  button3.innerText=goTown;
+  Text.innerText= "You enter the store.you see a sign that says \*store\".";
+}
+
+function goCave() {
+    console.lod("Going to Store.");
+  }
+
+function fightDragon() {
+    console.lod("fight dragon.");
+  }
+
+  function buyHealth() {
+    console.lod("buy health.");
+  }
+
+  function buyWeapon() {
+    console.lod("buy weapon");
+  }
+
+  function goTown() {
+    console.lod("go town.");
+  }*/
+
+
+
+  
